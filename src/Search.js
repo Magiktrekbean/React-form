285668dbf49e8ec32d8c32f903e094ff86c1790b
Magiktrekbean <153703739+Magiktrekbean@users.noreply.github.com>
2025-03-30 21:18:20 -0400

    Initial commit

---
+++ src/Search.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+
+export default function Search() {
+  let [query, setQuery] = useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    if (query.trim() === "") {
+      alert(`Please Enter a City`);
+    } else {
+      alert(`Searching...`);
+      alert(`${query}`);
+    }
+  }
+
+  function updateQuery(event) {
+    setQuery(event.target.value);
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="search" placeholder="Type a query" onChange={updateQuery} />
+      <input type="submit" value="Search" />
+    </form>
+  );
+}
